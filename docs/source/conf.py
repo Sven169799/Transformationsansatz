f47c26b4c746febaee96e53c6d0cbec502a147df
Sven <sven.graupner@icloud.com>
2025-07-09 13:47:13 +0200

    edit project, copyright, author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Transformationsansatz'
+copyright = '2025, abat AG'
+author = 'abat AG'
 
 release = '0.1'
 version = '0.1.0'
